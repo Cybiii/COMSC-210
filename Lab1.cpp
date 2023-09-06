51ed24e977f067a36a9d451beeb9a1880ad62757
Sebastian <110711768+Cybiii@users.noreply.github.com>
2023-09-05 21:36:51 -0700

    Lab1

---
+++ Lab1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int syracuse(int n, int countdown, int &highest){
+
+}
+
+int main(){
+    int start_n;
+    int lim;
+    bool start_invalid = true;
+    bool limit_invalid = true;
+
+    cout << "Enter the starting number: ";
+    cin >> start_n;
+
+    cout << "Enter the limit of terms: "
+
+    while(start_invalid){
+
+    }
+
+    while(limit_invalid){
+
+    }
+
+    if (syracuse()){
+
+    }
+
+    cout << "Highest number reached: " << highest << endl;
+
+    return 0;
+
+}
